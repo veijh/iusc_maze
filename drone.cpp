f87996b865965dd7aeff2d1589d38f6546e22d3a
veijh <844088636@qq.com>
2023-09-21 21:47:57 +0800

    重规划

--- drone.cpp
+++ drone.cpp
@@ -3,13 +3,25 @@
 //
 #include "drone.h"
 
-void Drone::plan(vector<Map> &map_vector, const int &src_id, const vector<int> &dst_id) {
+Drone::Drone(double x, double y)
+{
+    cur_node_id = 0;
+    cur_x = x;
+    cur_y = y;
+    cur_node_ptr = merged_path.begin();
+}
+
+int Drone::plan(vector<Map> &map_vector, const int &src_id, const vector<int> &dst_id) {
     vector<int> path;
     vector<int> possible_map_id;
     vector<int> candidate_path_id;
     if(!path_list.empty())
     {
         path_list.clear();
+    }
+    if(!merged_path.empty())
+    {
+        merged_path.clear();
     }
     for (auto &map:map_vector)
     {
@@ -36,7 +48,7 @@ void Drone::plan(vector<Map> &map_vector, const int &src_id, const vector<int> &
         // 删去其他候选路径
         for(int i = 0; i<candidate_path_id.size(); i++)
         {
-            if(path_list.at(i).at(count) != merged_path.back())
+            if(path_list.at(candidate_path_id.at(i)).at(count) != merged_path.back())
             {
                 candidate_path_id.erase(i+candidate_path_id.begin());
             }
@@ -52,5 +64,58 @@ void Drone::plan(vector<Map> &map_vector, const int &src_id, const vector<int> &
             break;
         }
     }
+    cur_node_ptr = merged_path.begin();
+}
+
+void Drone::fly_to_node(int id, Map &map)
+{
+    cout << cur_node_id << " -> ";
+    dsr_x = map.node.at(id).x;
+    dsr_y = map.node.at(id).y;
+    cout << id << endl;
+}
+
+int Drone::next_node()
+{
+    if(merged_path.end() > 1+cur_node_ptr)
+    {
+        return *(cur_node_ptr+1);
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int Drone::is_reached()
+{
+    double dx = cur_x - dsr_x;
+    double dy = cur_y - dsr_y;
+    if(dx*dx+dy*dy < dis_th*dis_th) return 1;
+    else return 0;
+}
+int Drone::is_reached(double x, double y)
+{
+    double dx = cur_x - x;
+    double dy = cur_y - y;
+    if(dx*dx+dy*dy< dis_th*dis_th) return 1;
+    else return 0;
+}
+
+bool Drone::is_next_node_reachable()
+{
+    return true;
+}
+
+bool Drone::is_next_node_reachable(Map &map)
+{
+    for(auto &edge:map.node.at(cur_node_id).node_edge)
+    {
+        if(edge.dst_id == next_node())
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
--- drone.h
+++ drone.h
@@ -9,17 +9,32 @@
 #include <vector>
 #include <list>
 #include <algorithm>
+#include <cmath>
 #include "maze_map.h"
 using namespace std;
+
+const double dis_th = 0.3;
 
 class Drone
 {
 public:
-    double current_x,current_y;
+    double cur_x,cur_y;
+    double dsr_x, dsr_y;
+
+    int cur_node_id;
+
     vector<vector<int>> path_list;
     vector<int> merged_path;
+    vector<int>::iterator cur_node_ptr;
+
+    Drone(double x=0.0, double y=0.0);
     bool is_next_node_reachable();
-    void plan(vector<Map> &map_vector, const int &src_id, const vector<int> &dst_id);
+    bool is_next_node_reachable(Map &map);
+    int plan(vector<Map> &map_vector, const int &src_id, const vector<int> &dst_id);
     vector<int> get_merged_path();
+    void fly_to_node(int id, Map &map);
+    int next_node();
+    int is_reached();
+    int is_reached(double x, double y);
 };
 #endif //IUSC_MAZE_DRONE_H
